800431984b396fbb5455aa7a0cc9b6266e215c69
miguelvalerio07 <miguelvalerio076@gmail.com>
2025-01-28 13:23:37 -0300

    build: instalando o flask

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from model.interface import Interface
+from flask import Flask, render_template
 
-def main():
-    Interface()
+app = Flask(__name__)
 
-if __name__ == '__main__':
-    main()
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/ola")
+def hello_world():
+    return "<p>Olá, mundo!</p>"
+
+# from model.interface import Interface
+
+# def main():
+#     Interface()
+
+# if __name__ == '__main__':
+#     main()
